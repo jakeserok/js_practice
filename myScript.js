e5b8a54bc5017d04749ac63b072aa4800a59de43
jakeserok <jakeserok@gmail.com>
2021-07-11 21:36:29 -0400

    Separated html and js into different files

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function myTestFunction() {
+  document.getElementById("test").innerHTML = "This is a test message!";
+}
